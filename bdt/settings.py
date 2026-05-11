532b49494744b7b49de120ebf4d8fd9fda96bfc9
Laura Pradelli <168731701+laurapradelli@users.noreply.github.com>
2026-05-12 00:20:13 +0200

    Update settings.py

--- bdt/settings.py
+++ bdt/settings.py
@@ -141,3 +141,11 @@ STATIC_ROOT = BASE_DIR / 'static/base'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com' #or another provider
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
